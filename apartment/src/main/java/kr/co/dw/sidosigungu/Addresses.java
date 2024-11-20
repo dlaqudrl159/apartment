d61d86ca79ca83c1fb7b4396b008bce9d6309d57
dlaqudrl159 <qkfkaxkrl@gmail.com>
2024-11-20 16:13:10 +0900

    auth 작업중

---
+++ apartment/src/main/java/kr/co/dw/sidosigungu/Addresses.java
@@ -0,0 +1,74 @@
+package kr.co.dw.sidosigungu;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Addresses {
+
+	private List<AddressElement> addresses;
+	
+	public Addresses(List<String> addresses) {
+        this.addresses = processAddresses(addresses);
+    }
+	
+	private List<AddressElement> processAddresses(List<String> addresses) {
+        return addresses.stream()
+                .map(AddressElement::new)
+                .distinct()
+                .collect(Collectors.toList());
+    }
+	
+	public List<AddressElement> getAddresses() {
+        return addresses;
+    }
+	
+	public static class AddressElement {
+        private String city;
+        private String district;
+
+        public AddressElement(String fullAddress) {
+            String[] parts = fullAddress.split(" ");
+            
+            if(parts.length <= 1) {
+            	this.city = "ERROR";
+            	this.district = "ERROR";
+            }else {
+            	if(parts[0].equals("세종특별자치시")) {
+                	this.city = parts[0];
+                	this.district = parts[0];
+                }else {
+                	this.city = parts[0];
+                    this.district = parts[1];
+                }
+            }
+            
+        }
+
+        public String getCity() {
+            return city;
+        }
+
+        public String getDistrict() {
+            return district;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            AddressElement that = (AddressElement) o;
+            return city.equals(that.city) && district.equals(that.district);
+        }
+
+        @Override
+        public int hashCode() {
+            return 31 * city.hashCode() + district.hashCode();
+        }
+
+        @Override
+        public String toString() {
+            return city + " " + district;
+        }
+    }
+	
+}
