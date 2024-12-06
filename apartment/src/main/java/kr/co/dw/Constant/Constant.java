09ee37ebc08de4e71537e9221a895221b50bd8f6
dlaqudrl159 <qkfkaxkrl@gmail.com>
2024-12-06 17:41:18 +0900

    상수 추가

--- apartment/src/main/java/kr/co/dw/Constant/Constant.java
+++ apartment/src/main/java/kr/co/dw/Constant/Constant.java
@@ -6,4 +6,30 @@ public class Constant {
 	
 	public static final Integer DELETE_YEAR = 13;
 	public static final int BATCH_SIZE = 1000;
+	
+	public static final String API_DEAL_AMOUNT = "dealAmount";
+	public static final String API_DEALING_GBN = "dealingGbn"; 
+	public static final String API_BUILD_YEAR = "buildYear"; 
+	public static final String API_DEAL_YEAR = "dealYear";
+	public static final String API_APT_DONG = "aptDong"; 
+	public static final String API_RGST_DATE = "rgstDate"; 
+	public static final String API_SLER_GBN = "slerGbn"; 
+	public static final String API_BUYER_GBN = "buyerGbn"; 
+	public static final String API_UMD_NM = "umdNm"; 
+	public static final String API_APT_NM = "aptNm"; 
+	public static final String API_DEAL_MONTH = "dealMonth"; 
+	public static final String API_DEAL_DAY = "dealDay"; 
+	public static final String API_EXCLU_USEAR = "excluUseAr"; 
+	public static final String API_ESTATE_AGENT_SGG_NM = "estateAgentSggNm"; 
+	public static final String API_JIBUN = "jibun"; 
+	//public static final String API_LAND_CD = "landCd"; 
+	public static final String API_FLOOR = "floor"; 
+	public static final String API_CDEAL_DAY = "cdealDay"; 
+	//public static final String API_CDEAL_TYPE = "cdealType"; 
+	public static final String API_ROAD_NM = "roadNm"; 
+	public static final String API_BONBUN = "bonbun"; 
+	public static final String API_BUBUN = "bubun"; 
+	public static final String API_ROAD_NM_BONBUN = "roadNmBonbun"; 
+	public static final String API_ROAD_NM_BUBUN = "roadNmBubun"; 
+	public static final String API_SGGCD = "sggCd"; 
 }
