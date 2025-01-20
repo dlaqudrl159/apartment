d73281962b7067af2837f88ed8b80e52b0915dbe
dlaqudrl159 <qkfkaxkrl@gmail.com>
2025-01-21 06:59:10 +0900

    메모리 누수 방지 코드 추가

--- apartment/src/main/java/kr/co/dw/Service/AutoData/Apt/AutoAptDataServiceImpl.java
+++ apartment/src/main/java/kr/co/dw/Service/AutoData/Apt/AutoAptDataServiceImpl.java
@@ -79,6 +79,11 @@ public class AutoAptDataServiceImpl implements AutoAptDataService {
 			List<AptTransactionDto> aptTransactionDtos = aptDataParserService.createSuccessedAptTransactionDtos(successProcessedAutoAptDataDtos);
 			batchProcessAptTransactionDtos(aptTransactionDtos, korSido, failProcessedAutoAptDataDtos, deleteDealYearMonth);
 			//batchProcessAptTransactionDtos(aptTransactionDtos, korSido);
+			
+			for(int i = 0 ; i < successProcessedAutoAptDataDtos.size() ; i++) {
+				successProcessedAutoAptDataDtos.get(i).setProcesedAptDatas(null);
+			}
+			
 			long heapSize = Runtime.getRuntime().totalMemory();
 			long heapMaxSize = Runtime.getRuntime().maxMemory();
 			long heapFreeSize = Runtime.getRuntime().freeMemory();
